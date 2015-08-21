b3fcb995daacbcf766342791d2ce56a59083c21b
Koo2000 <koo2000@gmail.com>
2015-08-22 03:56:50 +0900

    dummy plan add

--- ie8-decorator/person-compile.js
+++ ie8-decorator/person-compile.js
@@ -1,17 +1,19 @@
-var fs = require("fs");
-var browserify = require("browserify");
-var babelify = require("babelify");
+var fs = require('fs');
+var browserify = require('browserify');
+var babelify = require('babelify');
 
 babelify.configure({
 	stage : 1
 });
 
-browserify("./js/decorator.es7.js", {
+browserify('./js/decorator.es7.js', {
 	debug : true,
-	standalone : 'Person'
+	standalone : 'Person',
 })
 .transform(babelify.configure({
-	stage : 1
-})).bundle().on("error", function(err) {
-	console.log("Error : " + err.message);
-}).pipe(fs.createWriteStream("dist/deco.js"));
+	stage : 1,
+	plugins : ['object-assign'],
+	loose: ['es6.classes', 'es6.modules']
+})).bundle().on('error', function(err) {
+	console.log('Error : ' + err.message);
+}).pipe(fs.createWriteStream('dist/decorator.js'));
